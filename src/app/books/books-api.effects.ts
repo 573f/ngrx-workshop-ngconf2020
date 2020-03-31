f98ea2e167054b4b4d55147aedd1be8525aaa820
Stephen Skubik-Peplaski <sskubikpeplaski@appriss.com>
2020-03-31 11:06:27 -0400

    Completed Effects

--- src/app/books/books-api.effects.ts
+++ src/app/books/books-api.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { map, mergeMap } from 'rxjs/operators';
+import { concatMap, exhaustMap, map, mergeMap } from 'rxjs/operators';
 
 import { BooksService } from '../shared/services';
 import { BooksApiActions, BooksPageActions } from './actions';
@@ -12,10 +12,49 @@ export class BooksApiEffects {
   loadBooks$ = createEffect(() =>
     this.actions$.pipe(
       ofType(BooksPageActions.enter),
-      mergeMap(() =>
+      exhaustMap(() =>
         this.booksService
           .all()
           .pipe(map(books => BooksApiActions.booksLoaded({ books: books })))
+      )
+    )
+  );
+
+  createBook$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(BooksPageActions.createBook),
+      concatMap(action =>
+        this.booksService
+          .create(action.book)
+          .pipe(map(newBook => BooksApiActions.bookCreated({ book: newBook })))
+      )
+    )
+  );
+
+  updateBook$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(BooksPageActions.updateBook),
+      concatMap(action =>
+        this.booksService
+          .update(action.bookId, action.book)
+          .pipe(
+            map(updatedBook =>
+              BooksApiActions.bookUpdated({ book: updatedBook })
+            )
+          )
+      )
+    )
+  );
+
+  deleteBook$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(BooksPageActions.deleteBook),
+      mergeMap(action =>
+        this.booksService
+          .delete(action.bookId)
+          .pipe(
+            map(() => BooksApiActions.bookDeleted({ bookId: action.bookId }))
+          )
       )
     )
   );
--- src/app/books/components/books-page/books-page.component.ts
+++ src/app/books/components/books-page/books-page.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { BookModel, BookRequiredProps } from 'src/app/shared/models';
-import { BooksService } from 'src/app/shared/services';
 
 import {
   selectActiveBook,
@@ -9,7 +8,7 @@ import {
   selectBookEarningsTotals,
   State
 } from '../../../shared/state';
-import { BooksApiActions, BooksPageActions } from '../../actions';
+import { BooksPageActions } from '../../actions';
 
 @Component({
   selector: 'app-books',
@@ -21,10 +20,7 @@ export class BooksPageComponent implements OnInit {
   currentBook$ = this.store.select(selectActiveBook);
   total$ = this.store.select(selectBookEarningsTotals);
 
-  constructor(
-    private booksService: BooksService,
-    private store: Store<State>
-  ) {}
+  constructor(private store: Store<State>) {}
 
   ngOnInit() {
     this.store.dispatch(BooksPageActions.enter());
@@ -48,24 +44,15 @@ export class BooksPageComponent implements OnInit {
 
   saveBook(bookProps: BookRequiredProps) {
     this.store.dispatch(BooksPageActions.createBook({ book: bookProps }));
-    this.booksService.create(bookProps).subscribe(book => {
-      this.store.dispatch(BooksApiActions.bookCreated({ book: book }));
-    });
   }
 
   updateBook(book: BookModel) {
     this.store.dispatch(
       BooksPageActions.updateBook({ bookId: book.id, book: book })
     );
-    this.booksService.update(book.id, book).subscribe(book => {
-      this.store.dispatch(BooksApiActions.bookUpdated({ book: book }));
-    });
   }
 
   onDelete(book: BookModel) {
     this.store.dispatch(BooksPageActions.deleteBook({ bookId: book.id }));
-    this.booksService.delete(book.id).subscribe(() => {
-      this.store.dispatch(BooksApiActions.bookDeleted({ bookId: book.id }));
-    });
   }
 }
